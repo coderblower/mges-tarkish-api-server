a11f3aabac77dda54581e2970a9ae3f2904d6023
CodeBlower <saifulrmail@gmail.com>
2024-11-02 00:15:53 +0600

    sdfs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 
