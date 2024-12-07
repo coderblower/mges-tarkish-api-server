c8e101f5425e7d33d942dec163475c4425a155cb
saiful110816 <saiful110816@acc.edu.bd>
2024-12-07 15:04:27 +0600

    sdf

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
     /*
