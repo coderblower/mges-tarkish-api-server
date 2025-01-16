2b4f41b5ce13168c4b04e8e2c6ac8492f7e6d06d
CodeBlower <saifulrmail@gmail.com>
2025-01-16 09:46:45 +0600

    fix

--- app/Models/Designation.php
+++ app/Models/Designation.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Designation extends Model
 {
     use HasFactory;
+
+    public function candidates()
+    {
+        return $this->hasMany(Candidate::class, 'designation_id');
+    }
 }
